aefca079503e285d0f39f9322f07c13cc55abf0e
Bhimgouda Patil <satvikp508@gmail.com>
2025-10-02 13:06:52 +0800

    minor change

--- client/src/components/LeverageTable.tsx
+++ client/src/components/LeverageTable.tsx
@@ -33,6 +33,7 @@ const LeverageTable = ({
               </th> */}
             </tr>
           </thead>
+
           <tbody className="bg-white divide-y divide-gray-200">
             {leveragePositions.filter((leveragePosition) => leveragePosition.positionId.slice(0, 42) !== "0x386fB147faDb206fb7Af36438E6ae1f8583f99dd".toLowerCase()).map((row, index) => (
               <tr
